d95a99695da1903d143b99228e835b56dafae5d9
AHMEDWAFA06 <ahmedfalcon06@gmail.com>
2023-08-10 17:58:40 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(){
+/**
+ *main - this is the main function
+ *
+ *return - always o
+ */
+int main(void){
 	puts ("\"Programming is like building a multilingual puzzle"): 
 return  (0);	
 }
