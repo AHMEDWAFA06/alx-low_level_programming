9a3157809a0dc06347bf0f5789cc4a6869fd3839
AHMEDWAFA06 <ahmedfalcon06@gmail.com>
2023-08-11 05:03:52 -0700

    a

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: A C  progam that print with printf function.
+ *
+ * Return:  Always 0  (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is apiece of art,\n");
+	return (0);
+}	
