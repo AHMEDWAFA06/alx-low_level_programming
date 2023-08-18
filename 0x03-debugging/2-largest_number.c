4cf72a7009847c014b46264ba5e30f7f8e26bb6a
AHMEDWAFA06 <ahmedfalcon06@gmail.com>
2023-08-17 17:36:30 -0700

    a

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,24 +10,21 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a >= b && a >= c)
-    {
-        largest = a;
-	printf("%d is the largest number\n", largest);
-    }
-    else if (b >= a && b >= c)
-    {
-        largest = b;
-	printf("%d is the largest number\n", largest);
-    }
-    else
-    {
-        largest = c;
-	printf("%d is the largest number\n", largest);
-    }
+	if (a >= b && a >= c)
+	{
+	largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }
 
